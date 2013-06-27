70b91b343cfcf9f6b769834967df69929d2dec5d
Nate Andorsky <nandorsky@gmail.com>
2013-06-27 12:02:31 -0400

    added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Ideacenter::Application.routes.draw do
+
+
+  root :to => 'pages#home'
+
+  get 'ideas' => 'pages#idea'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
