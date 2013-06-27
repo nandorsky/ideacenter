1e2a22b797dcda4b27b902f3d0ae89149614fada
Nate Andorsky <nandorsky@gmail.com>
2013-06-27 11:54:28 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideacenter::Application.config.secret_token = '216abd7f0868417c95016132758aa7aa06cb1ec882d6bd3b39788b9453f142ac62247da1d1cbd614f7d5ab6c303a6697d52f58be6439282990d6552b44b5511f'
